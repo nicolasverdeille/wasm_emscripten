f09114634927e9ac330419c5977b03b257baf6bd
Nico <nico_verdeille@hotmail.com>
2023-03-12 00:00:25 +0100

    return string from rust and smaller JS file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use std::ffi::{c_char, CStr};
+static COIN: &CStr = unsafe { CStr::from_bytes_with_nul_unchecked(b"Vive les petits canards\0") };
+
 #[no_mangle]
 pub extern "C" fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-fn main() {}
+#[no_mangle]
+pub extern "C" fn coin() -> *const c_char {
+    COIN.as_ptr()
+}
+
+pub fn main() {
+}
